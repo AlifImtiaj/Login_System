8ae102fc293ca6a4f30a55720f48b50c95fea5c5
Alif Imtiaj <lolalif412@gmail.com>
2025-07-09 01:31:09 +0600

    Add project files.

---
+++ Login_System.cpp
@@ -0,0 +1,12 @@
+﻿// Login_System.cpp : Defines the entry point for the application.
+//
+
+#include "Login_System.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
